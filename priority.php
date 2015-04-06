68135ecfade799e67d398d6cc8475f01bc52ee33
Quoi d'Autre <dev.fabrice@yahoo.fr>
2015-04-06 20:47:36 +0200

    Update priority.php

--- priority.php
+++ priority.php
@@ -1,3 +1,13 @@
 <?php
-
+function count_configurations($a, $b, $c, $n) {
+    $counted = 0;
+    for ($i = 0; $i <= $a; $i++) {
+        for ($j = 0; $j <= $b; $j++) {
+            for ($k = 0; $k <= $c; $k++) {
+                if ($i + $j + $k == $n) { $counted++; }
+            }
+        }
+    }
+    echo $counted;
+}
 ?>
