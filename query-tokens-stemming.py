e564d39b1ed8901a20abc0d799b33ba947311cc1
Quoi d'Autre <dev.fabrice@yahoo.fr>
2015-04-09 20:36:25 +0200

    Create query-tokens-stemming.py

---
+++ query-tokens-stemming.py
@@ -0,0 +1,11 @@
+def token_stemming(tokens, suffixes):
+    hash_s = {}
+    for s in suffixes:
+        hash_s[s] = 1
+    for t in tokens:
+        for i in xrange(len(t)):
+            if not hash_s.get(t[i:]) is None:
+                print t[:i]
+                break
+        else:
+            print t
