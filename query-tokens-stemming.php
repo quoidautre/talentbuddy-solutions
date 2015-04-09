c011ab3f674947d4d15ca5410b96f5219ca598fd
Quoi d'Autre <dev.fabrice@yahoo.fr>
2015-04-09 20:35:48 +0200

    Update query-tokens-stemming.php

--- query-tokens-stemming.php
+++ query-tokens-stemming.php
@@ -1,11 +1,83 @@
-def token_stemming(tokens, suffixes):
-    hash_s = {}
-    for s in suffixes:
-        hash_s[s] = 1
-    for t in tokens:
-        for i in xrange(len(t)):
-            if not hash_s.get(t[i:]) is None:
-                print t[:i]
-                break
-        else:
-            print t
+<?php
+function token_stemming($tokens, $suffixes) {
+   foreach( $tokens as $t ){
+        $c = $t;
+        foreach( $suffixes as $s ){
+            $s = preg_quote($s);
+            $suf = preg_replace("/(".$s."$)/S","",$t);
+            $c = (strlen($c)>strlen($suf))?$suf:$c;
+        }
+        echo $c."\n";
+    }    
+}
+?>
+
+
+<?php
+function token_stemming($tokens, $suffixes) {
+    // Write your code here.
+    $out = [];
+    usort($suffixes, function($a, $b) {
+        return strlen($b) - strlen($a);
+    });
+    
+    foreach ($tokens as $token) {
+        $pattern = '/(' . implode('|', $suffixes) . ')$/';
+        $out[] = preg_replace($pattern, '', $token);
+    }
+    $out = implode("\n", $out);
+    echo $out;
+}
+?>
+
+<?php
+function token_stemming($tokens, $suffixes) {
+    function cmp($a, $b) {
+        return strlen($b)-strlen($a);
+    }
+
+    // sort suffixes in descending length
+    usort($suffixes, "cmp");
+
+    foreach ($tokens as $t) {
+        $matches = false;
+        foreach ($suffixes as $s) {
+            if (preg_match("/(.*){$s}$/", $t, $matches)) {
+                echo $matches[1] . PHP_EOL;
+                break;
+            }
+        }
+        
+        if (!$matches)
+            echo $t . PHP_EOL;
+    }
+}
+?>
+
+<?php
+function compare_len($a, $b)
+{
+    return (strlen($a)<strlen($b));
+}
+function remove_suffix(&$token, $key, $suffixes)
+{
+    usort($suffixes, "compare_len");
+    foreach ($suffixes as $suffix)
+    {
+        if ($token == $suffix) //add stupid test to make this evaluation work
+        {
+            $token = "";
+            break;
+        }
+        if ($token!=basename($token, $suffix)) 
+            {
+                $token = basename($token, $suffix);
+                break;
+            }
+    }
+}
+function token_stemming($tokens, $suffixes) {
+    array_walk($tokens,'remove_suffix', $suffixes);
+    echo implode("\n", $tokens);
+}
+?>
